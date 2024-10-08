fe28ccb79d2a9f4934ded39bb779228bbf6c68d2
R.G. <frontend-rob@proton.me>
2024-10-08 22:23:19 +0200

    updated theme

--- js/theme.js
+++ js/theme.js
@@ -23,8 +23,8 @@ function applyTheme(theme) {
 
 function updateMetaThemeColor(theme) {
     const themeColorMetaTag = document.querySelector('meta[name="theme-color"]');
-    const lightThemeColor = 'var(--bg-base-100)';
-    const darkThemeColor = 'var(--bg-base-100)';
+    const lightThemeColor = '#f5f4f0';
+    const darkThemeColor = '#292927';
 
     themeColorMetaTag.setAttribute('content', theme === 'dark' ? darkThemeColor : lightThemeColor);
 }
